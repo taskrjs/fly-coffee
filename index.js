6f5d91138cda0ddde4fd3e2b5c0a9558848e4bc9
Jorge Bucaran <jbucaran@me.com>
2015-08-04 16:58:43 +0900

    add tests :boom:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function () {
-  return this.filter("coffee", (src, opts) => {
-    try { return require("coffee-script").compile(src, opts) }
+  return this.filter("coffee", function (source, options) {
+    try { return require("coffee-script").compile(source, options) }
     catch (e) { throw e }
-  })
+  }, { ext: ".js" })
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,13 @@
 const test = require("tape").test
+const coffee = require("../")
 
-test("fly-coffee", function (t) {
-  t.ok(true, "ok")
-  t.end()
+test("fly-coffee", (t) => {
+  t.plan(3)
+  coffee.call({
+    filter: function (name, transform, options) {
+      t.equal(name, "coffee", "add coffee filter")
+      t.ok(/this.a/.test(transform("@a")), "coffee transform")
+      t.equal(options.ext, ".js", "extension is .js")
+    }
+  })
 })
