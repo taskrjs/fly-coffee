61b07caee6746865fa62b3ed31243682c1e841f1
Luke Edwards <luke.edwards05@gmail.com>
2017-02-21 08:30:06 -0800

    update & add tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,48 @@
-const test = require("tape").test
-const coffee = require("../")
+const join = require("path").join
+const test = require("tape")
+const Fly = require("fly")
+
+const dir = join(__dirname, "fixtures")
+const tmp = join(__dirname, "tmp")
 
 test("fly-coffee", (t) => {
-  t.plan(3)
-  coffee.call({
-    filter: function (name, transform) {
-      const result = transform("@a")
-      t.equal(name, "coffee", "add coffee filter")
-      t.ok(/this\.a/.test(result.code), "coffee transform")
-      console.log(result)
-      t.equal(result.ext, ".js", "extension is .js")
+  t.plan(6)
+
+  const fly = new Fly({
+    plugins: [
+      require("../"),
+      require("fly-clear")
+    ],
+    tasks: {
+      *foo(f) {
+        t.ok("coffee" in fly.plugins, "attach the `coffee()` plugin to fly")
+
+        yield f.source(`${dir}/foo.coffee`).coffee().target(tmp)
+
+        const want = yield f.$.read(`${dir}/foo.js`, "utf8")
+        const sent = yield f.$.read(`${tmp}/foo.js`, "utf8")
+
+        t.ok(sent, "creates a `.js` file")
+        t.equal(sent, want, "compile the `.coffee` contents correctly")
+
+        yield f.clear(tmp)
+      },
+      *bar(f) {
+        yield f.source(`${dir}/bar.coffee`).coffee({ sourceMap: true }).target(tmp)
+
+        const map = yield f.$.read(`${tmp}/bar.js.map`)
+        t.ok(map, "compiler receives options/config")
+
+        const arr = yield f.$.expand(`${tmp}/*`)
+        t.equal(arr.length, 2, "creates a file and external sourcemap")
+
+        const str = yield f.$.read(`${tmp}/bar.js`, "utf8")
+        t.ok(/sourceMappingURL/.test(str), "via `sourceMap`; attach `sourceMappingURL` comment")
+
+        yield f.clear(tmp)
+      }
     }
   })
+
+  fly.serial(["foo", "bar"])
 })
